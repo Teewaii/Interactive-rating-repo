e77fa2904454c9d1fe11d3003396425715c657f5
Taiye <taiyeajimati@gmail.com>
2022-06-25 15:14:41 +0400

    Final update

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ let ratings = document.querySelector("span")
 let mainClass = document.querySelector(".container")
 let submitBtn = document.querySelector("button")
 let submitSec = document.querySelector(".submit")
+let alertMsg = document.querySelector(".alert")
 
-for( let i=0; i<ratingOption.length; i++){
-ratingOption[i].addEventListener('click', ratingSelection)
+
+for (let i = 0; i < ratingOption.length; i++) {
+    ratingOption[i].addEventListener('click', ratingSelection)
 }
 
-function ratingSelection(){
-// console.log(this.innerText)
-ratings.textContent =ratingOption[this.innerText-1].innerText
-
+function ratingSelection() {
+s
+    ratings.textContent = ratingOption[this.innerText - 1].innerText
 }
-
 
 submitBtn.addEventListener('click', submitButton)
-function submitButton(){
-mainClass.classList.add("visible")
- submitSec.classList.toggle("show-class")
- 
+
+function submitButton() {
+    if (ratings.textContent > 0) {
+        mainClass.classList.add("visible")
+        submitSec.classList.toggle("show-class")
+    } else {
+        alertMsg.classList.toggle('alert')
+    }
 }

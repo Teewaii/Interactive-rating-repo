4cf4e5ee5c81d21e7533be0e92cbdf336221850d
Taiye Ajimati <taiyeajimati@gmail.com>
2022-06-26 15:28:08 +0400

    scale update

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-let ratingOption = document.querySelectorAll(".rating-options")
-let ratings = document.querySelector("span")
-let mainClass = document.querySelector(".container")
-let submitBtn = document.querySelector("button")
-let submitSec = document.querySelector(".submit")
-let alertMsg = document.querySelector(".alert")
+const ratingOption = document.querySelectorAll(".rating-options");
+const ratings = document.querySelector("span");
+const mainClass = document.querySelector(".container");
+const submitBtn = document.querySelector("button");
+const submitSec = document.querySelector(".submit");
+const alertMsg = document.querySelector(".alert");
 
 
 for (let i = 0; i < ratingOption.length; i++) {
-    ratingOption[i].addEventListener('click', ratingSelection)
-
+    ratingOption[i].addEventListener('click', ratingSelection);
 }
 
 function ratingSelection() {
-    ratings.textContent = ratingOption[this.innerText - 1].innerText
+    ratings.textContent = ratingOption[this.innerText - 1].innerText;  
+    
 }
 
-submitBtn.addEventListener('click', submitButton)
+submitBtn.addEventListener('click', submitButton);
 
 function submitButton() {
     if (ratings.textContent > 0) {
-        mainClass.classList.add("visible")
-        submitSec.classList.toggle("show-class")
+        mainClass.classList.add("visible");
+        submitSec.classList.toggle("show-class");
     } else {
-        alertMsg.classList.toggle('alert')
+        alertMsg.classList.toggle('alert');       
+        alertMsg.innerText ='Please select a valid rating value';
     }
 }
-
-// ratingOption[1].addEventListener('click', function onClick(event) {
-//     event.target.style.backgroundColor = 'salmon';
-
-// });
